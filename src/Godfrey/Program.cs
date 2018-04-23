5ac0a1ecbed1843a87666dcc2b4dc5eb7b03d12c
Nick Strohm <nick.strohm99@gmail.com>
2018-04-23 20:26:49 +0200

    NuGet Package Update + Cleanup

--- src/Godfrey/Program.cs
+++ src/Godfrey/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Fluent.Task;
 using Godfrey.Models.Context;
 using Newtonsoft.Json;
-using TaskScheduler = Fluent.Task.TaskScheduler;
 
 namespace Godfrey
 {
@@ -35,12 +32,6 @@ namespace Godfrey
 
             (await DatabaseContextFactory.CreateAsync(Butler.ButlerConfig.ConnectionString)).Dispose();
 
-            Scheduler = TaskScheduler.Instance().Start();
-            Schedule.Instance(CasinoCronAction)
-                    .SetTimeHour(6)
-                    .SetFrequencyTime(TimeSpan.FromDays(1))
-                    .RunLoop(Scheduler);
-
             var tskl = new List<Task>();
             for (var i = 0; i < Butler.ButlerConfig.ShardCount; i++)
             {
@@ -52,21 +43,6 @@ namespace Godfrey
             await Task.WhenAll(tskl);
 
             await Task.Delay(-1);
-        }
-
-        private static async void CasinoCronAction(object parameter)
-        {
-            Console.WriteLine("Adding 15 coins to every user that was active in the last two days");
-            using (var uow = await DatabaseContextFactory.CreateAsync(Butler.ButlerConfig.ConnectionString))
-            {
-                foreach (var user in uow.Users.Where(x => DateTime.UtcNow - x.LastCasinoCommandIssued <=
-                                                          TimeSpan.FromDays(2)))
-                {
-                    user.Coins += 15;
-                }
-
-                await uow.SaveChangesAsync();
-            }
         }
     }
 }
